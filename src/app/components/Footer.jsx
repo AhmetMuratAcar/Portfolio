f5deeec5e46bedc9704c7a7de1d3281ed697bab3
Ahmet Murat Acar <neistersenat@gmail.com>
2024-05-23 01:00:56 -0700

    Styling change.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer z-10 text-gray-600 bg-[#FFFFFF] w-screen">
+    <footer className="footer z-10 text-black bg-[#FFFFFF] w-screen">
       <div className="container py-12 flex justify-center items-center mx-auto" style={{ maxWidth: "100vw" }}>
         <p className="text-center">༼ つ ◕_◕ ༽つ Made with ❤️ on caffeine ༼ つ ◕_◕ ༽つ</p>
       </div>
